795724f566ed922f1fc56fd040dd56942c7c4636
ms-zaman <dev.mszaman@gmail.com>
2026-05-17 14:35:22 +0600

    style: update brand casing in navigation logo

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -11,7 +11,7 @@ const Navigation = () => {
     <nav className="nav">
       <div className="nav-inner">
         <Link to="/" className="nav-logo">
-          <span>ms</span>.zaman
+          <span>MS</span>.Zaman
         </Link>
 
         {/* Desktop Navigation */}
